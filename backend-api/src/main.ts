41aaf8d0cebc931d5e85a853c27546a0f4b6b51d
Hensigeraldi <friends.wapoga@gmail.com>
2026-05-08 02:41:21 +0700

    access file main.ts

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -4,7 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      "https://hiveeduanalytics.site",
+      "https://www.hiveeduanalytics.site",
+      "http://localhost:3001"
+    ],
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    credentials: true,
+    allowedHeaders: "Content-Type, Accept, Authorization",
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
